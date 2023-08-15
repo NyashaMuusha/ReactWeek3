3c0f0a0d87161d1b634ab6da1778b957dee0cef9
Nyasha Ivy Peace <nyashamuusha@gmail.com>
2023-08-15 16:59:55 +0200

    added app.js file

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,13 @@
 import React ,{useState}from 'react';
 import Header from './Header';
 import Speakers from './Speakers';
-import {data} from './../../speakerData'
+
 const App = () => {
     const [theme, setTheme] = useState('light');
   return ( 
     <div className={theme === 'light' ? 'container-fluid light': "container-fluid dark"}>
         <Header theme={theme}/>
-      <Speakers data={data} theme={theme} setTheme={setTheme}/>
+      <Speakers theme={theme} setTheme={setTheme}/>
     </div>
     
    );
